c4a026b786a5b5057a35173dcf828c4a3e5ce6fa
TheGrudge <kostadinovskiviktor@gmail.com>
2015-09-06 19:06:48 +0300

    final version

---
+++ BirthdayNotifier/src/birthdaynotifier/fragments/SplashFragment.java
@@ -0,0 +1,21 @@
+package birthdaynotifier.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.birthdaynotifier.R;
+
+public class SplashFragment extends Fragment{
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, 
+	        ViewGroup container, Bundle savedInstanceState) {
+	    View view = inflater.inflate(R.layout.splash, 
+	            container, false);
+	    return view;
+	}
+}
